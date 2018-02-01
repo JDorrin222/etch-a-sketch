443f0adade4e25d6cf018c3c652d79cc5b44f80f
Jena Dorrin <JRose81688@gmail.com>
2018-02-01 14:23:55 -0500

    final functionality etch a sketch

--- main.js
+++ main.js
@@ -13,13 +13,17 @@ function shake() {
 
 // function to create grid
 function createGrid() {
-  let gridSize = 100;
-
-  for(i = 0; i < gridSize*gridSize; i++){
+  let newGridSize = document.getElementById("gridSizeForm").value;
+  let squareWidth = 960 / newGridSize;
+  for(i = 0; i < newGridSize * newGridSize; i++){
     const square=document.createElement("div");
     square.id="grid-square";
     square.addEventListener("mouseover", function() {this.style.backgroundColor = "red"})
     document.getElementById("squares").appendChild(square);
+    square.style.width = squareWidth + "px";
+    square.style.height = squareWidth + "px";
   }
+console.log(squareWidth);
 }
+
 // function to change color when hover
